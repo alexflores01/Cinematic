b2ed305bebf1e7949fece700451e90da1c9768ee
alexflores <flores7alex@gmail.com>
2026-04-20 01:13:36 -0700

    fix: images in TextScene

--- script.js
+++ script.js
@@ -92,9 +92,9 @@ class TextScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('topPetals', 'assets/redpetalsfallingtop.png');
-        this.load.image('leftPetals', 'assets/redpetalsfallingleft.png');
-        this.load.image('rightPetals', 'assets/redpetalsfallingright.png');
+        this.load.image('topPetals', 'assets/redpetalsfallingtop.PNG');
+        this.load.image('leftPetals', 'assets/redpetalsfallingleft.PNG');
+        this.load.image('rightPetals', 'assets/redpetalsfallingright.PNG');
 
     }
 
